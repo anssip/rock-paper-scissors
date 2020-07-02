e7d67aa03e6019e8abe11a7ff7fdca9cbfd53e3a
Anssi Piirainen <api@iki.fi>
2020-07-02 16:49:45 +0300

    Add CSS modules support Fixes #1

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const webpack = require('webpack');
+const path = require('path');
+const context = path.resolve(__dirname, 'src');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,6 +10,38 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
+      },
+      {
+        include: path.resolve(__dirname, './src'),
+        loaders: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
+              sourceMap: true,
+            },
+          },
+        ],
+        test: /\.css$/,
+      },
+      {
+        include: path.resolve(__dirname, './src'),
+        loader: 'babel-loader',
+        query: {
+          plugins: [
+            '@babel/transform-react-jsx',
+            [
+              'react-css-modules',
+              {
+                context,
+              },
+            ],
+          ],
+        },
+        test: /\.js$/,
       },
     ],
   },
